1f3280c5d6f4fe76f13c28fb5f68e0dfb901cc28
Scott Miller <scott@millergeek.xyz>
2020-06-03 16:31:09 -0400

    ubdate brew

--- bin/install-macos.sh
+++ bin/install-macos.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo 'installing brew packages'
+brew update
 brew tap liamg/tfsec
 brew install tfenv tflint terraform-docs pre-commit liamg/tfsec/tfsec coreutils
 brew upgrade tfenv tflint terraform-docs pre-commit liamg/tfsec/tfsec coreutils
