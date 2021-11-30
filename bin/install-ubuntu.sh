6ca997b7eedaef0d133872f25a3aaadd14070b1e
Steven B <genseb7@gmail.com>
2021-11-30 11:31:33 -0500

    fix terraform-docs install (#40)
    * Update install-ubuntu.sh
    
    * rm old terraform-docs arg
    
    * Update .pre-commit-config.yaml
    
    pre-commit does not install terraform and it's dependencies, I'm trying to skip those hooks. see https://pre-commit.ci/#configuration-skip

--- bin/install-ubuntu.sh
+++ bin/install-ubuntu.sh
@@ -3,8 +3,17 @@
 echo 'installing dependencies'
 sudo apt install python3-pip gawk &&\
 pip3 install pre-commit
-tfdocs_latest_dl_url=$(curl -sL https://api.github.com/repos/terraform-docs/terraform-docs/releases/latest | grep -o -E "https://.+?-linux-amd64" | tail -n1)
-curl -L "$tfdocs_latest_dl_url" > terraform-docs && chmod +x terraform-docs && sudo mv terraform-docs /usr/bin/
+
+# terraform docs
+mkdir tmp
+cd tmp
+curl -Lo ./terraform-docs.tar.gz https://github.com/terraform-docs/terraform-docs/releases/download/v0.16.0/terraform-docs-v0.16.0-$(uname)-amd64.tar.gz
+tar -xzf terraform-docs.tar.gz
+chmod +x terraform-docs
+sudo mv terraform-docs /usr/bin/
+cd ..
+rm -rf tmp
+
 curl -L "$(curl -sL https://api.github.com/repos/terraform-linters/tflint/releases/latest | grep -o -E "https://.+?_linux_amd64.zip")" > tflint.zip && unzip tflint.zip && rm tflint.zip && sudo mv tflint /usr/bin/
 env GO111MODULE=on go get -u github.com/liamg/tfsec/cmd/tfsec
 git clone https://github.com/tfutils/tfenv.git ~/.tfenv || true
