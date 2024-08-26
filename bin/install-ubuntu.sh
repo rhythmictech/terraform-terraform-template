81c87945ddb87b9a6e91af2bb39a18a4fef70b42
Cris Daniluk <cdaniluk@rhythmictech.com>
2024-08-26 15:11:05 -0400

    fix trivy

--- bin/install-ubuntu.sh
+++ bin/install-ubuntu.sh
@@ -31,5 +31,5 @@ pre-commit init-templatedir ~/.git-template
 echo 'installing terraform with tfenv'
 tfenv install
 
-wget https://github.com/aquasecurity/trivy/releases/download/v0.49.1/trivy_0.54.1_Linux-64bit.deb
+wget https://github.com/aquasecurity/trivy/releases/download/v0.54.1/trivy_0.54.1_Linux-64bit.deb
 sudo dpkg -i trivy_0.54.1_Linux-64bit.deb
